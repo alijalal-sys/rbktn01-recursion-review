6adf451448ab40e914a46e0a6e7957cad5ce0231
alijalal-sys <ajmabilal@gmail.com>
2019-11-18 19:43:03 +0100

    Completed Bare minimum Requirements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	var arr = [];
+	function deepSearch(elm){
+		console.log(elm, elm.classList.contains(className))
+		if(elm.classList.contains(className) && arr.indexOf(elm) < 0){
+			arr.push(elm);
+		}
+		for(var child of elm.childNodes){
+			console.log(child, child.classList)
+			if(child.classList !== undefined){
+				if(child.classList.contains(className) && arr.indexOf(child) < 0){
+					arr.push(child);
+				}
+			}
+			if(child.childNodes.length > 0){
+				deepSearch(child);
+			}
+		}
+	}
+	deepSearch(document.body);
+	return arr;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,62 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
+function stringifyJSON(obj){
+	if(typeof obj !== 'object' || obj == null){
+		if(typeof obj === 'string'){
+			return '"' + obj + '"';
+		}
+		return String(obj);
+	}
+	if(Array.isArray(obj)){
+		return stringifyArray(obj)
+	}
+	if(Object.keys(obj).length === 0){
+		return "{}";
+	}
+	return stringJSON(obj);
+}
 
-// but you don't so you're going to write it from scratch:
+var stringJSON = function(obj) {
 
-var stringifyJSON = function(obj) {
-  // your code goes here
-};
+	  var str = '{';
+	  for(var key in obj){
+	  	if(typeof obj[key] === 'function' || obj[key]=== undefined){
+	  		continue;
+	  	}
+	  	if(Array.isArray(obj[key])){
+	  		str += '"' + key + '":' + stringifyArray(obj[key])+',';
+	  	} else if(typeof obj[key]==='object' && obj[key]!==null){
+	  		str += '"' + key +'":' +stringJSON(obj[key]) + ','
+	  	}else if(typeof obj[key]==='string'){
+	  		str += '"' + key +'":' +'"' + obj[key] + '"' + ','
+	  	}else
+	  		str += '"' + key +'":' +obj[key] +','
+	  }
+
+	  if(str[str.length - 1] === ',')
+	  	str = str.slice(0,str.length - 1)
+	  
+	  return str + '}';
+	};
+
+
+	function stringifyArray(array) {
+		str = '[';
+		for(var elm of array){
+			if(typeof elm === 'function' || elm === undefined){
+	  		continue;
+	  	}
+			if(Array.isArray(elm)){
+				str += stringifyArray(elm) + ',';
+			} else if(typeof elm === 'object')
+				str +=  stringJSON(elm) + ','
+			else if(typeof elm === 'string'){
+				str += '"' + elm + '"' + ','
+			} else
+				str += elm + ','
+		}
+
+		if(str[str.length - 1] === ',')
+			str = str.slice(0,str.length-1);
+		str += ']';
+
+		return str;
+	}
